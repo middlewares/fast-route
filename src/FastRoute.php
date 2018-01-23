a83eeccefe26479366b63e643fadb4ad9721534e
Eric Colinet <ecolinet@gmail.com>
2018-01-23 20:19:40 +0100

    use official PSR-15 packages

--- src/FastRoute.php
+++ src/FastRoute.php
@@ -4,11 +4,11 @@ declare(strict_types = 1);
 namespace Middlewares;
 
 use FastRoute\Dispatcher;
-use Interop\Http\Server\MiddlewareInterface;
-use Interop\Http\Server\RequestHandlerInterface;
 use Middlewares\Utils\Factory;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 
 class FastRoute implements MiddlewareInterface
 {
