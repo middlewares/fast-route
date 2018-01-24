c05de897dba6a2bdba83f898071840c39b3e1914
oscarotero <oom@oscarotero.com>
2018-01-24 21:21:17 +0100

    Code style fix

--- src/FastRoute.php
+++ src/FastRoute.php
@@ -4,11 +4,11 @@ declare(strict_types = 1);
 namespace Middlewares;
 
 use FastRoute\Dispatcher;
-use Psr\Http\Server\MiddlewareInterface;
-use Psr\Http\Server\RequestHandlerInterface;
 use Middlewares\Utils\Factory;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 
 class FastRoute implements MiddlewareInterface
 {
