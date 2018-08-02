b1e5a3006e88130bc975581de15a4e3c5b930883
oscarotero <oom@oscarotero.com>
2018-08-02 20:56:51 +0200

    updated utils to 2.1

--- src/FastRoute.php
+++ src/FastRoute.php
@@ -4,8 +4,7 @@ declare(strict_types = 1);
 namespace Middlewares;
 
 use FastRoute\Dispatcher;
-use Middlewares\Utils\Factory;
-use Psr\Http\Message\ResponseFactoryInterface;
+use Middlewares\Utils\Traits\HasResponseFactory;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
@@ -13,6 +12,8 @@ use Psr\Http\Server\RequestHandlerInterface;
 
 class FastRoute implements MiddlewareInterface
 {
+    use HasResponseFactory;
+
     /**
      * @var Dispatcher FastRoute dispatcher
      */
@@ -22,11 +23,6 @@ class FastRoute implements MiddlewareInterface
      * @var string Attribute name for handler reference
      */
     private $attribute = 'request-handler';
-
-    /**
-     * @var ResponseFactoryInterface
-     */
-    private $responseFactory;
 
     /**
      * Set the Dispatcher instance.
@@ -47,16 +43,6 @@ class FastRoute implements MiddlewareInterface
     }
 
     /**
-     * Set the response factory to return the error responses.
-     */
-    public function responseFactory(ResponseFactoryInterface $responseFactory): self
-    {
-        $this->responseFactory = $responseFactory;
-
-        return $this;
-    }
-
-    /**
      * Process a server request and return a response.
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
@@ -64,13 +50,11 @@ class FastRoute implements MiddlewareInterface
         $route = $this->router->dispatch($request->getMethod(), $request->getUri()->getPath());
 
         if ($route[0] === Dispatcher::NOT_FOUND) {
-            $responseFactory = $this->responseFactory ?: Factory::getResponseFactory();
-            return $responseFactory->createResponse(404);
+            return $this->createResponse(404);
         }
 
         if ($route[0] === Dispatcher::METHOD_NOT_ALLOWED) {
-            $responseFactory = $this->responseFactory ?: Factory::getResponseFactory();
-            return $responseFactory->createResponse(405)->withHeader('Allow', implode(', ', $route[1]));
+            return $this->createResponse(405)->withHeader('Allow', implode(', ', $route[1]));
         }
 
         foreach ($route[2] as $name => $value) {
